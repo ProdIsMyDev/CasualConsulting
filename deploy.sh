c841dc2104b474303e0ca35bc997e7c88800377e
cko <chris@casualcode.org>
2020-11-30 00:22:01 +0100

    adding deployment

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+set -e
+npm build
+cd dist
+echo 'casualconsulting.de' > CNAME
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:ProdIsMyDev/CasualConsulting.git master:gh-pages
+cd -
